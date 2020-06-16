32676a5780b7bebd09b27cbde30adcf9eb23c5bd
Kazuki Nishiura <dsbnatut@gmail.com>
2020-06-15 21:23:12 -0700

    Add endpoint for searching users

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -6,4 +6,13 @@ class FollowersController < ApplicationController
         @rules = Rule.where(user_id: current_user.id).order("created_at desc")
         @followers = UserFollower.where(user_id: current_user.id).order("score desc").limit(100)
     end
+
+    def search
+        sanitized_query = "%#{UserFollower.sanitize_sql_like(params["query"])}%"
+        followers = UserFollower
+            .where(user_id: current_user.id)
+            .where("name LIKE :query OR screen_name LIKE :query", query: sanitized_query)
+            .order("score desc").limit(100)
+        render json: { followers: followers }
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get '/auth/twitter/callback', to: 'sessions#create'
   get '/logout', to:   'sessions#destroy'
-  resources :followers
+  resources :followers, only: [:index] do
+    collection do
+      get :search
+    end
+  end
 
   root 'home#top'
 end
