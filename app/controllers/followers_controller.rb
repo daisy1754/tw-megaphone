48201bf1001c93a2f9b41891aa9bc3d66bf05717
Kazuki Nishiura <dsbnatut@gmail.com>
2020-06-15 21:05:54 -0700

    base html for followers page

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,9 +1,9 @@
 class FollowersController < ApplicationController
     def index 
-#         current_user = User.first
+         current_user = User.first
         # TODO check score version
 
-        
+        @rules = Rule.where(user_id: current_user.id).order("created_at desc")
         @followers = UserFollower.where(user_id: current_user.id).order("score desc").limit(100)
     end
 end
