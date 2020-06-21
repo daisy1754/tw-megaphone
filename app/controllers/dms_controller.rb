eed7d032267a1e39de771c103826126d846fa1d5
Kazuki Nishiura <dsbnatut@gmail.com>
2020-06-20 20:09:11 -0700

    Use test links in test dms

--- app/controllers/dms_controller.rb
+++ app/controllers/dms_controller.rb
@@ -1,9 +1,11 @@
 class DmsController < ApplicationController
     def new
-        puts params
         @current_user = current_user
         @num_follower = UserFollower.where(user_id: current_user.id).count()
         @prefill = "Hi ${follower_name}, please pardon me for the sudden message. I'm contacting to you because I consider closing my twitter account, and I would like to get your email address so I can send my update. If you agree to provide me an email, please open ${email_link}. If you do not want to receive further message from me, please go ${optout_link}."
+        user_id = "test-#{current_user.uid}"
+        @email_link = url_for(controller: :emails, action: :show, id: user_id)
+        @optout_link = url_for(controller: :emails, action: :optout, email_id: user_id)
     end
 
     def create
@@ -18,9 +20,10 @@ class DmsController < ApplicationController
     end
 
     def send_me
+        user_id = "test-#{current_user.uid}"
         text = params["text"].sub("${follower_name}", current_user.user_name)
-        text = text.sub("${email_link}", "TODO link")
-        text = text.sub("${optout_link}", "TODO link")
+        text = text.sub("${email_link}", url_for(controller: :emails, action: :show, id: user_id))
+        text = text.sub("${optout_link}", url_for(controller: :emails, action: :optout, email_id: user_id))
 
         client = Twitter::REST::Client.new do |config|
             config.consumer_key        = ENV['API_KEY']
