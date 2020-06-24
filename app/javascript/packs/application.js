252e384a3880981be7c150572c7bdcaf81374215
Kazuki Nishiura <dsbnatut@gmail.com>
2020-06-23 22:57:51 -0700

    revert part of change in 8aedc18a9fac42708009a131da09c6ccd7caa30a

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,21 +20,10 @@ require("channels")
 import jquery, { type } from 'jquery';
 window.$ = window.jquery = jquery;
 
-
-import 'bootstrap/js/dist/util'
-import 'bootstrap/js/dist/alert'
-import 'bootstrap/js/dist/button'
-import 'bootstrap/js/dist/carousel'
-import 'bootstrap/js/dist/collapse'
-import 'bootstrap/js/dist/dropdown'
-import 'bootstrap/js/dist/modal'
-import 'bootstrap/js/dist/popover'
-import 'bootstrap/js/dist/scrollspy'
-import 'bootstrap/js/dist/tab'
-import 'bootstrap/js/dist/tooltip'
+import 'bootstrap'
 
 import '@fortawesome/fontawesome-free/js/all'
-import 'src/application.scss'
+import '../src/application.scss'
 
 function hideAndShow(toHideSelector, toShowSelector) {
   $(toHideSelector).hide();
